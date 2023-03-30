47870d154bde6335feb502e2f718d0588ddbb028
Zach Wood-Doughty <zachwooddoughty@gmail.com>
2023-03-30 11:56:25 -0500

    script for running colab locally

---
+++ run_local_colab.sh
@@ -0,0 +1,9 @@
+pip install -q --upgrade jupyter_http_over_ws 
+
+jupyter serverextension enable --py jupyter_http_over_ws
+
+jupyter notebook \
+  --NotebookApp.allow_origin='https://colab.research.google.com' \
+  --port=8888 \
+  --NotebookApp.port_retries=0 \
+  --no-browser
