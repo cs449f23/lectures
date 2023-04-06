94b892fd8e595e0ff82cb13821855a0237ac09a2
Zach Wood-Doughty <zachwooddoughty@gmail.com>
2023-04-06 15:23:24 -0500

    prettier plotting

--- utils/plotting.py
+++ utils/plotting.py
@@ -109,3 +109,88 @@ def plot_decision_surface(model=None,
 
     if ax is None:
         plt.show()
+
+
+def compute_bounds(features):
+    min1, max1 = features[:, 0].min()-1, features[:, 0].max()+1
+    min2, max2 = features[:, 1].min()-1, features[:, 1].max()+1
+    return (min1, max1, min2, max2)
+
+
+def plot_decision_regions(
+        features, targets, model,
+        axis=None, transform=None,
+        bounds=None,
+        title='Decision Surface'):
+    """
+    Slightly different plotting approach than above. Used in backprop demo.
+
+    This function produces a single plot containing a scatter plot of the
+    features, targets, and decision region of the model.
+
+    Args:
+        features (np.ndarray): 2D array containing real-valued inputs.
+        targets (np.ndarray): 1D array containing binary targets.
+        model: a learner with .predict() method
+        axis: the axis on which to plot. If None, create a new plot
+        title: title of the plot
+    Returns:
+        None (plots to the active figure)
+    """
+
+    # define bounds of the domain
+    if bounds is None:
+        min1, max1, min2, max2 = compute_bounds(features)
+    else:
+        min1, max1, min2, max2 = bounds
+
+    # define grid for visualizing decision regions
+    x1grid = np.arange(min1, max1, 0.1)
+    x2grid = np.arange(min2, max2, 0.1)
+
+    xx, yy = np.meshgrid(x1grid, x2grid)
+
+    # flatten grid to a vector
+    r1, r2 = xx.flatten(), yy.flatten()
+    r1, r2 = r1.reshape((len(r1), 1)), r2.reshape((len(r2), 1))
+
+    # horizontally stack vectors to create x1,x2 input for the model
+    grid = np.hstack((r1,r2))
+
+    # if we're transforming the features, do that now
+    #     this allows xx and yy to still be in 2D for the visualization
+    #     but grid has been transformed so it matches up with the fit model
+    if transform is not None:
+        grid = transform(grid)
+
+    # generate predictions over grid
+    yhat = model.predict(grid)
+
+    # reshape the predictions back into a grid
+    zz = yhat.reshape(xx.shape)
+
+
+    if axis is None:
+        fig, axis = plt.subplots()
+
+    # plot the grid of x, y and z values as a surface
+    binary_cmap = matplotlib.colors.ListedColormap(['#9ce8ff', '#ffc773'])
+    axis.contourf(xx, yy, zz, cmap=binary_cmap, alpha=0.7)
+
+    # plot "negative" class:
+    row_idx_neg = np.where(targets < 0.5)[0]
+    axis.scatter(
+        features[row_idx_neg, 0], features[row_idx_neg, 1],
+        label='negative')
+
+    # plot "positive" class:
+    row_idx_pos = np.where(targets > 0.5)[0]
+    axis.scatter(
+        features[row_idx_pos, 0], features[row_idx_pos, 1],
+        label='positive')
+
+    axis.set_title(title)
+    axis.set_xlim(min1, max1)
+    axis.set_ylim(min2, max2)
+
+    axis.legend(loc="upper left")
